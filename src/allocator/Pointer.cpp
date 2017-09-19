cef3227b197cf7cca8e566e98c5b74a556b94068
Ivan Krylov <krylov.r00t@gmail.com>
2017-09-19 19:36:42 +0300

    oops, make it compile again

--- src/allocator/Pointer.cpp
+++ src/allocator/Pointer.cpp
@@ -4,10 +4,11 @@ namespace Afina {
 namespace Allocator {
 
 Pointer::Pointer() : ptr(nullptr) {}
-Pointer::Pointer(const Pointer &p_) { ptr = p_->ptr; }
+// shouldn't I worry about double-free at this point?
+Pointer::Pointer(const Pointer &p_) { ptr = p_.ptr; }
 Pointer::Pointer(Pointer &&p_) {
-    ptr = p_->ptr;
-    p_->ptr = nullptr;
+    ptr = p_.ptr;
+    p_.ptr = nullptr;
 }
 
 // TODO: check for self-assignment or something; see "= default"
