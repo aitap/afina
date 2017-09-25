85c0d12ab6b73cee97636ebac34148d9f1a18b58
aitap <krylov.r00t@gmail.com>
2017-09-25 23:31:22 +0300

    Simple allocator: implement the necessary constructors

--- src/allocator/Simple.cpp
+++ src/allocator/Simple.cpp
@@ -2,13 +2,14 @@
 #include <afina/allocator/Pointer.h>
 #include <afina/allocator/Simple.h>
 
+#include <cassert> // not supplying enough memory to the constructor will kill the program
 #include <memory>
 
 namespace Afina {
 namespace Allocator {
 
 // chosen by a fair dice roll
-const std::uint32_t block_magic = 0xd700e698, footer_magic = 0xe26ab656;
+static const std::uint32_t block_magic = 0xd700e698, footer_magic = 0xe26ab656;
 
 // header for a memory block
 struct block {
@@ -16,7 +17,8 @@ struct block {
     std::size_t size;
     block *next;
     block *next_free;
-    block() : magic(block_magic), next(nullptr), next_free(nullptr){};
+    block(std::size_t size_, block *next_ = nullptr, block *next_free_ = nullptr)
+        : magic(block_magic), size(size_), next(next_), next_free(next_free_){};
 };
 
 // lives at fixed offset, contains global info about our allocator
@@ -24,13 +26,19 @@ struct footer {
     std::uint32_t magic;
     block *first_free;
     std::size_t num_allocated;
-    footer() : magic(footer_magic){};
+    footer(void *base) : magic(footer_magic), first_free((block *)base), num_allocated(0){};
 };
 
 Simple::Simple(void *base, size_t size) : _base(base), _base_len(size) {
-    if (sizeof(footer) + sizeof(block) <= size)
-        throw AllocError(AllocErrorType::NoMemory, "Can't find place for the global header");
-    // TODO
+    // they told me not to throw from constructor, so I didn't
+    assert(size > sizeof(block) + sizeof(footer));
+    /* Leave enough space for:
+     * - the block descriptor
+     * - the footer at the end of the memory area
+     * - one void* to point at the allocated block */
+    block *head = new (base) block{size - sizeof(footer) - sizeof(footer) - sizeof(void *)};
+    footer *ftr = new ((char *)base - sizeof(footer)) footer{base};
+    // Constructors did their magic, we seem to have a valid linked list
 }
 
 /**
