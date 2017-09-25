fe02346f2403dea1465d17eeacbb7dba88a8298f
Ivan Krylov <krylov.r00t@gmail.com>
2017-09-25 18:11:50 +0300

    minor cleanup

--- src/allocator/Simple.cpp
+++ src/allocator/Simple.cpp
@@ -1,6 +1,6 @@
-#include <afina/allocator/Simple.h>
 #include <afina/allocator/Error.h>
 #include <afina/allocator/Pointer.h>
+#include <afina/allocator/Simple.h>
 
 #include <memory>
 
@@ -14,22 +14,23 @@ const std::uint32_t block_magic = 0xd700e698, footer_magic = 0xe26ab656;
 struct block {
     std::uint32_t magic;
     std::size_t size;
-    block * next;
-    block * next_free;
-    block() : magic(block_magic), next(nullptr), next_free(nullptr) {};
+    block *next;
+    block *next_free;
+    block() : magic(block_magic), next(nullptr), next_free(nullptr){};
 };
 
 // lives at fixed offset, contains global info about our allocator
 struct footer {
     std::uint32_t magic;
-    block* first_free;
+    block *first_free;
     std::size_t num_allocated;
     footer() : magic(footer_magic){};
 };
 
 Simple::Simple(void *base, size_t size) : _base(base), _base_len(size) {
-	if (sizeof(footer)+sizeof(block) <= size) throw AllocError(AllocErrorType::NoMemory, "Can't find place for the global header");
-	// TODO
+    if (sizeof(footer) + sizeof(block) <= size)
+        throw AllocError(AllocErrorType::NoMemory, "Can't find place for the global header");
+    // TODO
 }
 
 /**
@@ -60,8 +61,8 @@ void Simple::defrag() {}
  * TODO: semantics
  */
 std::string Simple::dump() const {
-	// I guess I am allowed to do std::string-related heap operations there
-	return "";
+    // I guess I am allowed to do std::string-related heap operations there
+    return "";
 }
 
 } // namespace Allocator
