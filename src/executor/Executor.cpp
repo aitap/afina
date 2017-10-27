5510c0f354d2f2acabf096df6fa11bf5dde82459
Ivan Krylov <krylov.r00t@gmail.com>
2017-10-27 19:31:13 +0300

    skeleton of the Executor class

---
+++ src/executor/Executor.cpp
@@ -0,0 +1,18 @@
+#include "afina/Executor.h"
+
+void perform(Afina::Executor *executor) { throw; }
+
+Afina::Executor::Executor(std::string /* wtf?! */, int size) {
+    std::lock_guard<std::mutex> lock{mutex};
+    for (int i = 0; i < size; i++)
+        threads.emplace(threads.end(), perform, this);
+}
+
+Afina::Executor::~Executor() {
+    std::lock_guard<std::mutex> lock{mutex};
+    if (state == State::kRun)
+        Stop(true);
+    if (state == State::kStopping)
+        for (std::thread &thread : threads)
+            thread.join();
+}
