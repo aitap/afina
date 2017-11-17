13366d949f91ec6f85a89f37a2bd0aa3f6841bd8
aitap <krylov.r00t@gmail.com>
2017-11-17 22:39:28 +0300

    bugfix: don't ignore the return value of Storage::Put
    for striped storage it may be false if the target bucket is empty
    (nothing to remove) but other buckets are full, so no insert is possible

--- src/execute/Set.cpp
+++ src/execute/Set.cpp
@@ -9,8 +9,7 @@ namespace Execute {
 // memcached protocol: "set" means "store this data".
 void Set::Execute(Storage &storage, const std::string &args, std::string &out) {
     std::cout << "Set(" << _key << "): " << args << std::endl;
-    storage.Put(_key, args);
-    out = "STORED";
+    out = storage.Put(_key, args) ? "STORED" : "NOT_STORED";
 }
 
 } // namespace Execute
