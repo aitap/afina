7c15fea5cccb3e975edf7fca022517bdec29fc40
Ivan Krylov <krylov.r00t@gmail.com>
2017-11-20 20:27:51 +0300

    minor fixes

--- src/coroutine/Engine.cpp
+++ src/coroutine/Engine.cpp
@@ -15,16 +15,15 @@ namespace Coroutine {
 void Engine::Store(context &ctx) {
     volatile char onmystack = 0;
     std::get<1>(ctx.Stack) = StackBottom - &onmystack;
-    if (std::get<0>(ctx.Stack))
+    if (std::get<0>(ctx.Stack)) // clean up previously used state, if any
         delete[] std::get<0>(ctx.Stack);
     std::get<0>(ctx.Stack) = new char[std::get<1>(ctx.Stack)];
     memcpy(std::get<0>(ctx.Stack), (const void *)&onmystack, std::get<1>(ctx.Stack));
-    // that's all we needed to do
 }
 
 void Engine::Restore(context &ctx) {
     volatile unsigned char onmystack = 0;
-    // don't clobber the state by function call
+    // don't clobber the state by longjmp function call
     if ((char *)&onmystack > StackBottom - std::get<1>(ctx.Stack)) {
         volatile uint64_t filler = 0;
         Restore(ctx); // this doesn't return, but don't let the compiler know about it
@@ -37,16 +36,17 @@ void Engine::Restore(context &ctx) {
 }
 
 void Engine::yield() {
-    if (alive) {
+    if (alive) { // run another coroutine, if any
         context *to_call = alive;
         alive->prev = nullptr;
         alive = alive->next;
-        return sched(to_call);
+        return sched(to_call); // overwrite the stack frame
     }
+    // nothing to do - return
 }
 
 // cur_routine is the caller, we should land in it when the callee returns
-// unless sched is called from run() or start(), then it's NULL
+// unless sched is called from run() or start(), then it's NULL and we'll land in idle_ctx
 void Engine::sched(void *routine_) {
     context *to_call = (context *)routine_;
     to_call->caller = cur_routine;
@@ -57,7 +57,7 @@ void Engine::sched(void *routine_) {
             return;
         }
     }
-    // else we should land in the idle context
+    // throw current frame away
     cur_routine = to_call;
     Restore(*to_call);
 }
